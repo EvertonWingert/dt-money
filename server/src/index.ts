0c36cf813d4da1096b3d31f5c1ea4d6e55d651ee
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-08-05 22:02:58 -0300

    add: prettier/eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,174 +1,178 @@
-import express from "express";
-import { Request, Response } from "express";
-import crypto from "crypto";
+import express from 'express';
+import { Request, Response } from 'express';
+import crypto from 'crypto';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 export type TransactionEntity = {
-	id: string;
-	amount: number;
-	type: string;
-	description: string;
-	category: string;
-	createdAt: Date;
+  id: string;
+  amount: number;
+  type: string;
+  description: string;
+  category: string;
+  createdAt: Date;
 };
 const transactions = <TransactionEntity[]>[];
 
 //List all transactions
-app.get("/v1/api/transactions", (req: Request, res: Response) => {
-	return res.status(200).json({
-		transactions,
-	});
+app.get('/v1/api/transactions', (req: Request, res: Response) => {
+  return res.status(200).json({
+    transactions,
+  });
 });
 
 //Show a specific transaction
-app.get("/v1/api/transactions/:id", (req: Request, res: Response) => {
-	const transaction = transactions.find((transaction) => transaction.id === req.params.id);
-	if (!transaction) {
-		return res.status(404).json({
-			message: "Transaction not found",
-		});
-	}
+app.get('/v1/api/transactions/:id', (req: Request, res: Response) => {
+  const transaction = transactions.find(
+    transaction => transaction.id === req.params.id,
+  );
+  if (!transaction) {
+    return res.status(404).json({
+      message: 'Transaction not found',
+    });
+  }
 
-	return res.status(200).json({
-		transaction,
-	});
+  return res.status(200).json({
+    transaction,
+  });
 });
 
 //Create a new transaction
-app.post("/v1/api/transactions", (req: Request, res: Response) => {
-	const { amount, type, description, category } = req.body;
+app.post('/v1/api/transactions', (req: Request, res: Response) => {
+  const { amount, type, description, category } = req.body;
 
-	if (!amount || !type || !description || !category) {
-		return res.status(422).json({
-			message: "Missing required fields (amount, type, description, category)",
-		});
-	}
+  if (!amount || !type || !description || !category) {
+    return res.status(422).json({
+      message: 'Missing required fields (amount, type, description, category)',
+    });
+  }
 
-	if (!Number.isInteger(amount)) {
-		return res.status(422).json({
-			message: "Amount must be an integer",
-		});
-	}
+  if (!Number.isInteger(amount)) {
+    return res.status(422).json({
+      message: 'Amount must be an integer',
+    });
+  }
 
-	if (typeof type !== "string") {
-		return res.status(422).json({
-			message: "Type must be a string",
-		});
-	}
+  if (typeof type !== 'string') {
+    return res.status(422).json({
+      message: 'Type must be a string',
+    });
+  }
 
-	if (type !== "income" && type !== "expense") {
-		return res.status(422).json({
-			message: "Type must be income or expense",
-		});
-	}
+  if (type !== 'income' && type !== 'expense') {
+    return res.status(422).json({
+      message: 'Type must be income or expense',
+    });
+  }
 
-	if (typeof description !== "string") {
-		return res.status(422).json({
-			message: "Description must be a string",
-		});
-	}
+  if (typeof description !== 'string') {
+    return res.status(422).json({
+      message: 'Description must be a string',
+    });
+  }
 
-	if (typeof category !== "string") {
-		return res.status(422).json({
-			message: "Category must be a string",
-		});
-	}
+  if (typeof category !== 'string') {
+    return res.status(422).json({
+      message: 'Category must be a string',
+    });
+  }
 
-	const id = crypto.randomBytes(16).toString("hex");
+  const id = crypto.randomBytes(16).toString('hex');
 
-	const transaction = {
-		id,
-		amount,
-		type,
-		description,
-		category,
-		createdAt: new Date(),
-	};
+  const transaction = {
+    id,
+    amount,
+    type,
+    description,
+    category,
+    createdAt: new Date(),
+  };
 
-	transactions.push(transaction);
+  transactions.push(transaction);
 
-	return res.status(201).json({
-		message: "Transaction created successfully",
-		data: transaction,
-	});
+  return res.status(201).json({
+    message: 'Transaction created successfully',
+    data: transaction,
+  });
 });
 
 //Update a transaction
-app.put("/v1/api/transactions/:id", (req: Request, res: Response) => {
-	const { id } = req.params;
-	const { amount, type, description, category } = req.body;
+app.put('/v1/api/transactions/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { amount, type, description, category } = req.body;
 
-	if (!amount || !type || !description || !category) {
-		return res.status(422).json({
-			message: "Missing required fields (amount, type, description, category)",
-		});
-	}
+  if (!amount || !type || !description || !category) {
+    return res.status(422).json({
+      message: 'Missing required fields (amount, type, description, category)',
+    });
+  }
 
-	if (!Number.isInteger(amount)) {
-		return res.status(422).json({
-			message: "Amount must be an integer",
-		});
-	}
+  if (!Number.isInteger(amount)) {
+    return res.status(422).json({
+      message: 'Amount must be an integer',
+    });
+  }
 
-	if (typeof type !== "string") {
-		return res.status(422).json({
-			message: "Type must be a string",
-		});
-	}
+  if (typeof type !== 'string') {
+    return res.status(422).json({
+      message: 'Type must be a string',
+    });
+  }
 
-	if (type !== "income" && type !== "expense") {
-		return res.status(422).json({
-			message: "Type must be income or expense",
-		});
-	}
+  if (type !== 'income' && type !== 'expense') {
+    return res.status(422).json({
+      message: 'Type must be income or expense',
+    });
+  }
 
-	if (typeof description !== "string") {
-		return res.status(422).json({
-			message: "Description must be a string",
-		});
-	}
+  if (typeof description !== 'string') {
+    return res.status(422).json({
+      message: 'Description must be a string',
+    });
+  }
 
-	if (typeof category !== "string") {
-		return res.status(422).json({
-			message: "Category must be a string",
-		});
-	}
+  if (typeof category !== 'string') {
+    return res.status(422).json({
+      message: 'Category must be a string',
+    });
+  }
 
-	//update
-	const transaction = transactions.find((t) => t.id === id);
-	if (!transaction) {
-		return res.status(404).json({
-			message: "Transaction not found",
-		});
-	}
+  //update
+  const transaction = transactions.find(t => t.id === id);
+  if (!transaction) {
+    return res.status(404).json({
+      message: 'Transaction not found',
+    });
+  }
 
-	transaction.amount = amount;
-	transaction.type = type;
-	transaction.description = description;
-	transaction.category = category;
+  transaction.amount = amount;
+  transaction.type = type;
+  transaction.description = description;
+  transaction.category = category;
 
-	return res.status(200).json({
-		transaction,
-	});
+  return res.status(200).json({
+    transaction,
+  });
 });
 
 //Delete a transaction
-app.delete("/v1/api/transactions/:id", (req: Request, res: Response) => {
-	const transaction = transactions.find((transaction) => transaction.id === req.params.id);
-	if (!transaction) {
-		return res.status(404).json({
-			message: "Transaction not found",
-		});
-	}
+app.delete('/v1/api/transactions/:id', (req: Request, res: Response) => {
+  const transaction = transactions.find(
+    transaction => transaction.id === req.params.id,
+  );
+  if (!transaction) {
+    return res.status(404).json({
+      message: 'Transaction not found',
+    });
+  }
 
-	transactions.slice(transactions.indexOf(transaction), 1);
+  transactions.slice(transactions.indexOf(transaction), 1);
 
-	return res.status(204).json({
-		message: "Transaction deleted successfully",
-	});
+  return res.status(204).json({
+    message: 'Transaction deleted successfully',
+  });
 });
 
 export default app;
--- server/test/transaction-routes.test.ts
+++ server/test/transaction-routes.test.ts
@@ -1,147 +1,153 @@
-import supertest from "supertest";
-import app, { TransactionEntity } from "../src/index";
+import supertest from 'supertest';
+import app, { TransactionEntity } from '../src/index';
 
-describe("Transaction Routes", () => {
-	const request = supertest(app);
+describe('Transaction Routes', () => {
+  const request = supertest(app);
 
-	let transactions: TransactionEntity[] = [];
+  let transactions: TransactionEntity[] = [];
 
-	beforeAll(async () => {
-		const { body } = await request.post("/v1/api/transactions").send({
-			amount: 100,
-			type: "income",
-			description: "Salary",
-			category: "Salary",
-		});
+  beforeAll(async () => {
+    const { body } = await request.post('/v1/api/transactions').send({
+      amount: 100,
+      type: 'income',
+      description: 'Salary',
+      category: 'Salary',
+    });
 
-		transactions.push(body.data);
-	});
+    transactions.push(body.data);
+  });
 
-	describe("GET /v1/api/transactions", () => {
-		test("should return all transactions", async () => {
-			const response = await request.get("/v1/api/transactions");
+  describe('GET /v1/api/transactions', () => {
+    test('should return all transactions', async () => {
+      const response = await request.get('/v1/api/transactions');
 
-			expect(response.status).toBe(200);
-			expect(response.body).toHaveProperty("transactions");
-		});
-	});
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('transactions');
+    });
+  });
 
-	describe("GET /v1/api/transactions/:id", () => {
-		test("should return a specific transaction", async () => {
-			const response = await request.get(`/v1/api/transactions/${transactions[0].id}`);
+  describe('GET /v1/api/transactions/:id', () => {
+    test('should return a specific transaction', async () => {
+      const response = await request.get(
+        `/v1/api/transactions/${transactions[0].id}`,
+      );
 
-			expect(response.status).toBe(200);
-			expect(response.body).toHaveProperty("transaction");
-		});
-		test("should return 404 if transaction does not exist", async () => {
-			const response = await request.get("/v1/api/transactions/101");
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('transaction');
+    });
+    test('should return 404 if transaction does not exist', async () => {
+      const response = await request.get('/v1/api/transactions/101');
 
-			expect(response.status).toBe(404);
-		});
-	});
+      expect(response.status).toBe(404);
+    });
+  });
 
-	describe("POST /v1/api/transactions", () => {
-		test("should create a transaction", async () => {
-			const response = await request.post("/v1/api/transactions").send({
-				amount: 100,
-				type: "income",
-				description: "Salary",
-				category: "Salary",
-			});
+  describe('POST /v1/api/transactions', () => {
+    test('should create a transaction', async () => {
+      const response = await request.post('/v1/api/transactions').send({
+        amount: 100,
+        type: 'income',
+        description: 'Salary',
+        category: 'Salary',
+      });
 
-			expect(response.status).toBe(201);
-			expect(response.body.message).toBe("Transaction created successfully");
-			expect(response.body.data.amount).toBe(100);
-			expect(response.body.data.type).toBe("income");
-			expect(response.body.data.description).toBe("Salary");
-			expect(response.body.data.category).toBe("Salary");
-		});
-		test("should return 422 if amount is not an integer", async () => {
-			const response = await request.post("/v1/api/transactions").send({
-				amount: "100",
-				type: "income",
-				description: "Salary",
-				category: "Salary",
-			});
-		});
-		test("should return 422 if type is not income or expense", async () => {
-			const response = await request.post("/v1/api/transactions").send({
-				amount: 100,
-				type: "outcome",
-				description: "Salary",
-				category: "Salary",
-			});
+      expect(response.status).toBe(201);
+      expect(response.body.message).toBe('Transaction created successfully');
+      expect(response.body.data.amount).toBe(100);
+      expect(response.body.data.type).toBe('income');
+      expect(response.body.data.description).toBe('Salary');
+      expect(response.body.data.category).toBe('Salary');
+    });
+    test('should return 422 if amount is not an integer', async () => {
+      const response = await request.post('/v1/api/transactions').send({
+        amount: '100',
+        type: 'income',
+        description: 'Salary',
+        category: 'Salary',
+      });
+    });
+    test('should return 422 if type is not income or expense', async () => {
+      const response = await request.post('/v1/api/transactions').send({
+        amount: 100,
+        type: 'outcome',
+        description: 'Salary',
+        category: 'Salary',
+      });
 
-			expect(response.status).toBe(422);
-		});
+      expect(response.status).toBe(422);
+    });
 
-		test("should return 422 if description is not a string", async () => {
-			const response = await request.post("/v1/api/transactions").send({
-				amount: 100,
-				type: "income",
-				description: 100,
-				category: "Salary",
-			});
+    test('should return 422 if description is not a string', async () => {
+      const response = await request.post('/v1/api/transactions').send({
+        amount: 100,
+        type: 'income',
+        description: 100,
+        category: 'Salary',
+      });
 
-			expect(response.status).toBe(422);
-		});
+      expect(response.status).toBe(422);
+    });
 
-		test("should return 422 if category is not a string", async () => {
-			const response = await request.post("/v1/api/transactions").send({
-				amount: 100,
-				type: "income",
-				description: "Salary",
-				category: 100,
-			});
+    test('should return 422 if category is not a string', async () => {
+      const response = await request.post('/v1/api/transactions').send({
+        amount: 100,
+        type: 'income',
+        description: 'Salary',
+        category: 100,
+      });
 
-			expect(response.status).toBe(422);
-		});
+      expect(response.status).toBe(422);
+    });
 
-		test("should return 422 if any field is missing", async () => {
-			const response = await request.post("/v1/api/transactions").send({
-				amount: 100,
-				type: "income",
-				description: "Salary",
-			});
+    test('should return 422 if any field is missing', async () => {
+      const response = await request.post('/v1/api/transactions').send({
+        amount: 100,
+        type: 'income',
+        description: 'Salary',
+      });
 
-			expect(response.status).toBe(422);
-		});
-	});
+      expect(response.status).toBe(422);
+    });
+  });
 
-	describe("PUT /v1/api/transactions/:id", () => {
-		test("should return 404 if transaction does not exist", async () => {
-			const response = await request.put("/v1/api/transactions/101").send({
-				amount: 200,
-				type: "income",
-				description: "Salary",
-				category: "Salary",
-			});
-			expect(response.status).toBe(404);
-		});
+  describe('PUT /v1/api/transactions/:id', () => {
+    test('should return 404 if transaction does not exist', async () => {
+      const response = await request.put('/v1/api/transactions/101').send({
+        amount: 200,
+        type: 'income',
+        description: 'Salary',
+        category: 'Salary',
+      });
+      expect(response.status).toBe(404);
+    });
 
-		test("should update a specific transaction", async () => {
-			const response = await request.put(`/v1/api/transactions/${transactions[0].id}`).send({
-				amount: 200,
-				type: "income",
-				description: "Salary",
-				category: "Salary",
-			});
+    test('should update a specific transaction', async () => {
+      const response = await request
+        .put(`/v1/api/transactions/${transactions[0].id}`)
+        .send({
+          amount: 200,
+          type: 'income',
+          description: 'Salary',
+          category: 'Salary',
+        });
 
-			expect(response.status).toBe(200);
-			expect(response.body).toHaveProperty("transaction");
-			expect(response.body.transaction.amount).toBe(200);
-		});
-	});
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('transaction');
+      expect(response.body.transaction.amount).toBe(200);
+    });
+  });
 
-	describe("DELETE /v1/api/transactions/:id", () => {
-		test("should return 404 if transaction does not exist", async () => {
-			const response = await request.delete("/v1/api/transactions/");
-			expect(response.status).toBe(404);
-		});
+  describe('DELETE /v1/api/transactions/:id', () => {
+    test('should return 404 if transaction does not exist', async () => {
+      const response = await request.delete('/v1/api/transactions/');
+      expect(response.status).toBe(404);
+    });
 
-		test("should delete a specific transaction", async () => {
-			const response = await request.delete(`/v1/api/transactions/${transactions[0].id}`);
-			expect(response.status).toBe(204);
-		});
-	});
+    test('should delete a specific transaction', async () => {
+      const response = await request.delete(
+        `/v1/api/transactions/${transactions[0].id}`,
+      );
+      expect(response.status).toBe(204);
+    });
+  });
 });
